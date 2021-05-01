a4d211431310d21d3a0cffb579ccd8a899afab6e
ykisana <ykisana@users.noreply.github.com>
2021-04-30 20:29:37 -0600

    fix millerrabin

--- millerRabin.h
+++ millerRabin.h
@@ -1,7 +1,8 @@
 // Miller-Rabin Primality test
 // this runs in O(klog^3n) time
 // long long used to account for large numbers
-
+// modeled from Geeks for Geeks:
+// https://www.geeksforgeeks.org/primality-test-set-3-miller-rabin/
 
 #include <stdlib.h>
 #include <math.h>
@@ -12,16 +13,22 @@
 using namespace std;
 bool millerTest(LL n, LL d);
 LL fast_exp(LL b, LL n);
+LL power(LL x, unsigned LL y, LL p);
 
 
-//from UAlberta Competitive Programming Club Repo
-LL fast_exp(LL b, LL n){
-    LL res = 1, x = b, p;
 
-    for(p = n; p; p >>= 1, x *= x)
-        if(p & 1) res *= x;
 
-    return res;
+// returns (x ^ y) % p
+LL power(LL x, LL y, LL p){
+    LL out = 1;
+    x = x % p;
+    while (y > 0){
+        if (y & 1)
+            out = (out*x) % p;
+        y = y>>1;
+        x = (x*x) % p;
+    }
+    return out;
 }
 
 
@@ -31,7 +38,10 @@ bool isPrime(LL n, LL k){
     // k is the number of rounds of testing
 
     //check base cases and if n is even
-    if (n == 1 || n == 2 || n == 3){
+    if (n <= 1 || n == 4){
+        return false;
+    }
+    if(n <= 3){
         return true;
     }
     if (n % 2 == 0){
@@ -40,7 +50,7 @@ bool isPrime(LL n, LL k){
     // calculate d, where n - 1 = d * 2^2
     LL d = n - 1;
     while (d%2 == 0){
-        d = d/2;
+        d /= 2;
     }
     // call millerTest k times
     // if it fails  even  once  out of k times , return  false , else  true
@@ -55,9 +65,11 @@ bool isPrime(LL n, LL k){
 
 bool millerTest(LL n, LL d){
     // generate random number in range [2, n-2]
-    LL a = rand() % (n-3) + 2;
-    // uue the random number to create x
-    LL x = fast_exp(a,d) % n;
+    LL a = rand() % (n-4) + 2;
+    // use the random number to create x
+    // x is (a ^ d) % n;
+    LL x = power(a,d,n);
+
 
     if(x == 1 || x == n -1){
         return true;
--- primalityBattle.cpp
+++ primalityBattle.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "millerRabin.h"
+#include "solovayStrassen.h"
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
     cout << isPrime(n,1);
+    cout << solovoyTest(n,1);
 }
